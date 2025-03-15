480b85d2417bce8fcbc30808b48988ff34ee0a64
coffeebreak-chicken <117181942+coffeebreak-chicken@users.noreply.github.com>
2025-03-16 01:39:24 +0900

    batダブルクリックで、同一階層のjavaを実行可能に

--- RunJava_bat/java_run.bat
+++ RunJava_bat/java_run.bat
@@ -15,10 +15,50 @@ echo %DROPFILE%
 echo %DROPFILE_NAME%
 echo %DROPFILE_EXTENSION%
 
+rem 実行中のバッチファイルの存在するパスを取得
+set SCRIPT_PATH=%~dp0
+
 rem D&Dではなく、そのまま実行された場合
 IF [%DROPFILE%] == [] (
     echo 実行するjavaファイルが指定されていません.
     echo 所定の場所に格納されているjavaファイルを対象にします.
+
+    rem 結果を表示
+    echo 実行中のバッチファイルのパス: %SCRIPT_PATH%
+
+    rem Javaファイル一覧を格納するための変数を初期化
+    set JAVA_FILES=
+    
+    rem 指定したパス内のファイルをループで処理
+    for %%f in ("%SCRIPT_PATH%*.java") do (
+        set JAVA_FILES=!JAVA_FILES! %%f
+    )
+    rem Javaファイル一覧を表示
+    if "!JAVA_FILES!"=="" (
+        echo javaファイルは存在しません.
+        pause
+        exit /b
+    ) else (
+        rem ファイル一覧を1つずつ処理
+        for %%a in (!JAVA_FILES!) do (
+            rem コンパイル
+            javac "%%a"
+            if %ERRORLEVEL% neq 0 (
+                echo コンパイルに失敗しました.
+                echo コンパイルファイル: "%%a"
+                pause
+                exit /b
+            )
+            
+            rem 実行するJava名を取得（拡張子を除いたもの）
+            set CLASS_NAME=%%~na
+            rem java実行
+            echo 実行クラス: !CLASS_NAME!
+            java -cp %SCRIPT_PATH% !CLASS_NAME!
+            pause
+            exit /b
+        ) 
+    )
 )
 
 rem D&Dされたファイルがjavaファイルでない場合 %~x1
@@ -31,13 +71,14 @@ if not "%DROPFILE_EXTENSION%"==".java" (
 rem javaコンパイル
 javac "%DROPFILE%"
 if %ERRORLEVEL% neq 0 (
-    echo コンパイルに失敗しました。
+    echo コンパイルに失敗しました.
     pause
     exit /b
 )
 
 rem java実行
 java %DROPFILE_NAME%
-
+pause
+exit /b
 
 pause
